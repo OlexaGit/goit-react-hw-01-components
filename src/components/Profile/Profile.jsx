1e311cc1332e8e73f8c819a6053523ccc564d748
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-05-18 23:45:36 +0300

    profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,1 +1,47 @@
 import PropTypes from 'prop-types';
+
+function Profile({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt={username} class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
+
+export default Profile;
